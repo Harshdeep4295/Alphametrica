520fcec736c753f273466833e62344eccd5627d8
harshdeep.singh <Harshdeep.singh@pasarpolis.com>
2021-04-25 19:43:51 +0530

    welcome fragment added.

--- app/src/main/java/com/xavi/thealphametrica/activity/MainActivity.java
+++ app/src/main/java/com/xavi/thealphametrica/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.xavi.thealphametrica;
+package com.xavi.thealphametrica.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.xavi.thealphametrica.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/xavi/thealphametrica/fragments/LoginFragment.java
+++ app/src/main/java/com/xavi/thealphametrica/fragments/LoginFragment.java
@@ -1,4 +1,4 @@
-package com.xavi.thealphametrica;
+package com.xavi.thealphametrica.fragments;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.xavi.thealphametrica.R;
 
 /**
  * A simple {@link Fragment} subclass.
